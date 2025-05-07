f9c89ee10b3de552cf2b8e83e7f81f3cf2fce7ad
robert <rebcave8@gmail.com>
2025-05-07 10:41:10 +0100

    added expenese list file

---
+++ lib/expenses_list.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:expenses_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
