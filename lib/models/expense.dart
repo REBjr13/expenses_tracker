9d18d643e1d6e1edaa9bc2f771bd5a26e603444b
robert <rebcave8@gmail.com>
2025-05-07 10:19:54 +0100

    used enum keyword

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:uuid/uuid.dart'; //generates unique ID
 
-const uuid =  Uuid();
+const uuid = Uuid();
+
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+} //creates a custom type "predefined allowed values"
 
 class Expense {
   final String title;
   final double amount;
   final DateTime date;
   final String id;
+  final Category category;
 
-  Expense({required this.title, required this.amount, required this.date})
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.id,
+    required this.category,
+  });
     : id = uuid.v4(); //to generate unique id
 }
