e499d261dbe96ac4225170d66553fb78a111d133
robert <rebcave8@gmail.com>
2025-05-07 10:13:43 +0100

    adjusted constructor

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,15 +1,13 @@
 import 'package:uuid/uuid.dart'; //generates unique ID
 
+const uuid =  Uuid();
 
 class Expense {
   final String title;
   final double amount;
   final DateTime date;
+  final String id;
 
-  Expense({
-    required this.id,
-    required this.title,
-    required this.amount,
-    required this.date,
-  });
+  Expense({required this.title, required this.amount, required this.date})
+    : id = uuid.v4(); //to generate unique id
 }
