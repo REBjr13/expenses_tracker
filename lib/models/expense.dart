157ac6506879e406b506ccec782d90981206a0d4
robert <rebcave8@gmail.com>
2025-05-07 09:41:49 +0100

    created class and imported a uuid package

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,15 @@
+import 'package:uuid/uuid.dart'; //generates unique ID
+
+
 class Expense {
-  
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
 }
