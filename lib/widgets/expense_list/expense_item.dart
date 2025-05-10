a26786d6294db99a6c5a9b36394ec0069b453c9f
robert <rebcave8@gmail.com>
2025-05-10 20:02:11 +0100

    fleshed out card widget

--- lib/widgets/expense_list/expense_item.dart
+++ lib/widgets/expense_list/expense_item.dart
@@ -4,12 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:expenses_tracker/models/expense.dart';
 
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem(this.expense,{super.key });
+  const ExpenseItem(this.expense, {super.key});
 
   final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: Text(expense.title));
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 18),
+        child: Column(
+          children: [
+            Text(expense.title),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}', //sets place value to 2//use \ to escape the above
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.account_balance_rounded),
+                    SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
