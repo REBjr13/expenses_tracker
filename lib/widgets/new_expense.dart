0b49271a1844824ab00f5413009642428f2f711d
robert <rebcave8@gmail.com>
2025-05-13 20:19:09 +0100

    added new textfield and button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,10 +14,11 @@ class _NewExpenseState extends State<NewExpense> {
   // }
 
   final _titleControl = TextEditingController();
-
+  final _amountControl = TextEditingController();
   @override
   void dispose() {
     _titleControl.dispose(); //always dispose texteditingcontrollers
+    _amountControl.dispose();
     super.dispose();
   }
 
@@ -33,14 +34,24 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: InputDecoration(label: Text("Title")),
           ),
+          SizedBox(),
+          TextField(
+            controller: _amountControl,
+            maxLength: 7,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(label: Text("Amount")),
+          ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
                   debugPrint(_titleControl.text);
+                  debugPrint(_amountControl.text);
                 },
                 child: Text("Save Expense"),
               ),
+              SizedBox(width: 10),
+              TextButton(onPressed: () {}, child: Text('Close')),
             ],
           ),
         ],
