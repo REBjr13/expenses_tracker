3f0ef59ded0f5343dbfbd1250efd0e3e23e3621c
robert <rebcave8@gmail.com>
2025-05-21 16:53:47 +0100

    intro to theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 34, 193, 60),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,//overrides some aspects
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const ExpensesTrack(),
       debugShowCheckedModeBanner: false,
     ),
