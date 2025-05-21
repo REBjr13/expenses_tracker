bc81dcf8e5c3fcd8446a3b70134c8179479db141
robert <rebcave8@gmail.com>
2025-05-21 17:22:26 +0100

    added font package and adjusted some theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 34, 193, 60),
@@ -11,9 +12,27 @@ void main() {
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,//overrides some aspects
+          backgroundColor:
+              kColorScheme.onPrimaryContainer, //overrides some aspects
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),//sometimes you use styleFrom others copyWith
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+           
+          )
+        )
       ),
       home: const ExpensesTrack(),
       debugShowCheckedModeBanner: false,
