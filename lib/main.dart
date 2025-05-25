51b5e94003ed35a95de496e14beb31ca00460f83
robert <rebcave8@gmail.com>
2025-05-25 22:25:34 +0100

    set code to lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import "package:flutter/services.dart";
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 34, 193, 60),
@@ -11,52 +12,56 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      // darkTheme: Theming().darkTheme,
-      // theme: Theming().theme,
-      // themeMode: ThemeMode.light, //system theme mode
-
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ), //sometimes you use styleFrom others copyWith
-        ),
-
-      ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor:
-              kColorScheme.onPrimaryContainer, //overrides some aspects
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ), //sometimes you use styleFrom others copyWith
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(
-            color: kColorScheme.onPrimaryContainer,
-            fontWeight: FontWeight.bold,
-            fontSize: 17,
+  WidgetsFlutterBinding.ensureInitialized();//locks orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        // darkTheme: Theming().darkTheme,
+        // theme: Theming().theme,
+        // themeMode: ThemeMode.light, //system theme mode
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ), //sometimes you use styleFrom others copyWith
           ),
         ),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor:
+                kColorScheme.onPrimaryContainer, //overrides some aspects
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ), //sometimes you use styleFrom others copyWith
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              color: kColorScheme.onPrimaryContainer,
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
+          ),
+        ),
+        home: const ExpensesTrack(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const ExpensesTrack(),
-      debugShowCheckedModeBanner: false,
-    ),
-  );
+    );
+  });
 }
+
